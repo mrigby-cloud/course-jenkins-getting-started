543724502317a4dd68fcfed646295ffb7ebff8d7
Matt Rigby <mrigby83@gmail.com>
2021-06-27 19:51:19 +0100

    updating jenkins version in docker

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/mrigby-cloud/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
@@ -18,13 +18,13 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
+                // changed {
+                //     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                //         body: "Please go to ${BUILD_URL} and verify the build", 
+                //         attachLog: true, 
+                //         compressLog: true, 
+                //         to: "test@jenkins",
+                //         recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
